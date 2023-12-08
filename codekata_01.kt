6a3677a247256d00ee50914e9dc56d09b24b5f0a
happynslow <148965865+happynslow@users.noreply.github.com>
2023-12-08 19:20:49 +0900

    Add files via upload

---
+++ codekata_01.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication2.Hotel
+
+fun main() {
+    class Solution {
+        fun solution(num1: Int, num2: Int): Int {
+            var answer: Int = 0
+            return num1-num2
+        }
+    }
+}
